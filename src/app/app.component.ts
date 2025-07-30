b2acad6af48d925a196cec5945dbf0d598501e0a
atdimkal <d.kalpidis@ergohellas.gr>
2025-07-30 11:23:07 +0300

    Created table component for showing results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+
+import type { InvestmentInput } from './investment-input.model';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
-  calculateInvestmentResults(
-    data : {
-      initialInvestment: number, 
-      duration: number, 
-      expectedReturn: number, 
-      annualInvestment: number}
-  ) {
+  resultData: 
+
+  onCalculateInvestmentResults(data : InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } = data;
     const annualData = [];
     let investmentValue = initialInvestment;
@@ -36,7 +37,7 @@ export class AppComponent {
       });
     }
 
-    return annualData;
+    console.log(annualData);
   }
 
 
